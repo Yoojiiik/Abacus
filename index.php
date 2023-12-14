4034b567fde0507f64efe89da5967c6f79e41e0e
unknown <mariakartseva.arch@gmail.com>
2023-12-14 22:49:38 +0300

    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require_once('head.php') ?>
+<body>
+<header class="header">
+    <?php require_once('header.php') ?>
+</header>
+   <main class="main">
+      <section class="history"></section>
+      <section class="mission"></section>
+      <section class="production"></section>
+      <section class="what"></section>
+      <section class="recomindation"></section>
+      <section class="customers"></section>
+      <section class="reviews"></section>
+</main>
+<footer class="footer">
+<?php require_once('footer.php') ?>
+</footer>
+</body>
+</html>
