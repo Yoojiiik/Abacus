6168096cdb5d90d183cd9fa58ce7cadd35184214
unknown <mariakartseva.arch@gmail.com>
2023-12-15 11:37:56 +0300

    !

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <?php require_once('header.php') ?>
 </header>
    <main class="main">
-      <section class="history"></section>
+      <section class="history">
+        <conteiner></conteiner>
+      </section>
       <section class="mission"></section>
       <section class="production"></section>
       <section class="what"></section>
