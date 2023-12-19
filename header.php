f5fdfc39bc129e9b70c81676f3a02836ca91ac77
unknown <mariakartseva.arch@gmail.com>
2023-12-20 00:40:54 +0300

    header

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -9,3 +10,68 @@
     <title>AbakusKids устройство</title>
     <meta name="description" content="Уникальное устройство для обучения счету для детей с ограниченными возможностями здоровья от AbacusKids" />
 </head>
+=======
+<div class="conteiner">
+   <div class="header-top__wrapper">
+      <div class="header-top__abacus">
+         <nav class="header-top__nav">
+               <ul class="header-top__menu">
+               <li class="header-top__item">
+                     <a href="#" class="header-top__link">Для кого</a>
+                  </li>
+                  <li class="header-top__item">
+                     <a href="#" class="header-top__link">О нас</a>
+                  </li>
+                  <li class="header-top__item">
+                     <a href="#" class="header-top__link">Задачи устройства</a>
+                  </li>
+                  <li class="header-top__item">
+                     <a href="#" class="header-top__link">Видео</a>
+                  </li>
+                  <li class="header-top__item">
+                     <a href="#" class="header-top__link">Отзывы</a>
+                  </li>
+                  <li class="header-top__item">
+                     <a href="#" class="header-top__link">Контакты</a>
+                  </li>
+               </ul>
+         </nav>
+      </div>
+     
+         <div class="header-top-menu-part-2">
+            <div class="left-part-menu-part-2">
+               <img src="./assets/pic/Abacus_logo.svg" alt="АбакусКидс логотип" class="abacus_logo">
+               <p class="centr_development">Центр интеллектуального развития</p>
+            </div>
+
+            <div class="right-part-menu-part-2">
+               <a href="#" class="header_mail">
+               <img src="./assets/icon/Mail.svg" alt="Кнопка для отправки сообщениий">
+               </a>
+               <a href="#" class="header_phone">
+               <img src="./assets/icon/solar_phone.svg" alt="Кнопка для звонка по телефону">
+               </a>
+               <a href="#" class="oko">
+               <img src="./assets/icon/Oko.svg" alt="Кнопка, включающая версию для слабовидящих">
+               </a>
+            </div>
+         </div>
+      
+   </div>
+</div>
+<div class="hamburger-menu"> 
+      <input id="menu__toggle" type="checkbox" />
+      <label class="menu__btn" for="menu__toggle">
+      <span></span>
+      </label>
+   
+      <ul class="menu__box">
+            <li><a class="menu__item" href="#">Для кого</a></li>
+            <li><a class="menu__item" href="#">О нас</a></li>
+            <li><a class="menu__item" href="#">Задачи устройства</a></li>
+            <li><a class="menu__item" href="#">Видео</a></li>
+            <li><a class="menu__item" href="#">Отзывы</a></li>
+            <li><a class="menu__item" href="#">Контакты</a></li>
+      </ul>
+   </div>
+>>>>>>> Stashed changes
