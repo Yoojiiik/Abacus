871e419904e8e14090bce18a7d18361d17299964
Anna Nosova <annetdangerous@gmail.com>
2023-12-20 17:41:50 +0300

    add block develop

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       <section class="for_who">
         <div class="for_who_block">
           <div class="for_who_title">
-            <h2>Для кого?</h2>
+            <h2 id="for_who">Для кого?</h2>
           </div>
           <div class="container-fluid">
             <div class="for_who_card row row-cols-1 row-cols-md-6 g-4">
@@ -63,6 +63,82 @@
         </div>
 
       </section>
+
+
+
+      <section class="develop">
+      <div class="develop_block">
+          <div class="develop_title">
+            <h2 id="develop">Развивает</h2>
+            <img src="assets/pic/develop/brain2.png" alt="здесь изображен мозг">
+          </div>
+          <div class="container-fluid">
+            <div class="develop_card row row-cols-1 row-cols-md-6 g-4">
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">ВНИМАНИЕ</h5>
+                  <p class="card-text">Устройство способствует развитию внимания</p>
+                </div>  
+                <div>
+                  <img src="assets/pic/develop/puzzle.png" class="rounded" class="card-img-bottom" alt="здесь изображен пазл">
+                </div>        
+                
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">ЛОГИКУ</h5>
+                  <p class="card-text">Устройство способствует развитию логики</p>
+                </div>
+                <img src="assets/pic/develop/girl.png" class="card-img-bottom" alt="здесь изображена девочка">
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">ПАМЯТЬ</h5>
+                  <p class="card-text">Устройство способствует развитию памяти</p>
+                </div>
+                <img src="assets/pic/develop/brain1.png" class="card-img-bottom" alt="здесь изображен мозг">
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">ОСЯЗАНИЕ</h5>
+                  <p class="card-text">Устройство способствует развитию осязания</p>
+                </div>
+                <img src="assets/pic/develop/touch1.png" class="card-img-bottom" alt="здесь изображено касание рукой">
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">МОТОРИКУ</h5>
+                  <p class="card-text">Устройство способствует развитию моторики</p>
+                </div>
+                <img src="assets/pic/develop/hand1.png" class="card-img-bottom" alt="здесь изображена рука, кубики и игрушка">
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">МЫШЛЕНИЕ</h5>
+                  <p class="card-text">Устройство способствует развитию мышления</p>
+                </div>
+                <img src="assets/pic/develop/head1.png" class="card-img-bottom" alt="здесь изображена голова">
+              </div>
+
+              <div class="card col-md-4 col-lg-2">
+                <div class="card-body">
+                  <h5 class="card-title">СЛУХОВОЕ ВОСПРИЯТИЕ</h5>
+                  <p class="card-text">Устройство способствует развитию слухового восприятия</p>
+                </div>
+                <img src="assets/pic/develop/ear1.png" class="card-img-bottom" alt="здесь изображено ухо">
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
 
 
       <section class="history">
